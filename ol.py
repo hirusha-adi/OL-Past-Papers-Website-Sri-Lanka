2ae4112c7b7fb7ebdd9f80fd81939034c3f1492c
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2022-01-13 22:04:41 +0530

    updates

--- ol.py
+++ ol.py
@@ -1,3 +1,17 @@
-from distutils.log import debug
-from src.app import app
-app.run("0.0.0.0", port=3332, debug=True)
+from sys import argv
+
+from src.server import app
+
+
+debug = False
+try:
+    if str(argv[1]).lower().startswith("d") or str(argv[1]).lower().startswith("t"):
+        debug = True
+except IndexError:
+    pass
+
+if debug:
+    print("[+] Debug Mode: On")
+
+
+app.run("0.0.0.0", port=3332, debug=debug)
